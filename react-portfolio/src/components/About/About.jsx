26c7fe529cde6c2f504cf414f025cc1ee8581396
mark19242 <mark19242@gmail.com>
2024-05-17 15:37:49 -0700

    mvp

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from "react"
 
-import styles from "./About.module.css";
-import { getImageUrl } from "../../utils";
+import styles from "./About.module.css"
+import { getImageUrl } from "../../utils"
 
 export const About = () => {
   return (
@@ -19,7 +19,8 @@ export const About = () => {
             <div className={styles.aboutItemText}>
               <h3>Crafting Digital Experiences</h3>
               <p>
-              Venturing into web development naturally evolved from my years of service to my country and community, blending my creative passions with a deep interest in technology. This field has become my canvas for innovation, allowing me to make a meaningful impact through cutting-edge solutions.
+                Venturing into web development naturally evolved from my years
+                of service to my country and community.
               </p>
             </div>
           </li>
@@ -28,7 +29,9 @@ export const About = () => {
             <div className={styles.aboutItemText}>
               <h3>A Diverse Path to Innovation</h3>
               <p>
-              My transition from emergency services and military to community coaching honed my adaptability and leadership, enriching my tech endeavors with unparalleled problem-solving and teamwork skills. These experiences grant me a unique perspective in crafting innovative technology solutions.
+                My transition from emergency services and military to community
+                coaching honed my adaptability and leadership, enriching my tech
+                endeavors.
               </p>
             </div>
           </li>
@@ -37,12 +40,14 @@ export const About = () => {
             <div className={styles.aboutItemText}>
               <h3>Beyond Code: Building Connections</h3>
               <p>
-              My transition into web development is driven by a deep-seated mission to serve and connect with others, harnessing digital innovation to bridge gaps. Every project is an opportunity to strengthen community bonds, turning technological solutions into platforms for meaningful human connections.
+                Every project is an opportunity to strengthen community bonds,
+                turning technological solutions into platforms for meaningful
+                human connections.
               </p>
             </div>
           </li>
         </ul>
       </div>
     </section>
-  );
-};
+  )
+}
